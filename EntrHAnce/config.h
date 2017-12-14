ff4168b4f1c708d2ae93706edf56dad5d507b216
Mark Coombes <mark@markcoombes.ca>
2017-12-14 14:16:50 -0500

    Configuration parameters for entry monitoring

---
+++ EntrHAnce/config.h
@@ -0,0 +1,60 @@
+/*   
+ * EntrHAnce
+ * Licensed under the MIT License, Copyright (c) 2017 marthoc
+ * 
+ * User-configurable Parameters 
+*/
+
+// Wifi Parameters
+
+#define WIFI_SSID "your-ssid-name"
+#define WIFI_PASSWORD "your-ssid-password"
+
+// Static IP Parameters
+
+#define STATIC_IP false
+#define IP 192,168,1,100
+#define GATEWAY 192,168,1,1
+#define SUBNET 255,255,255,0
+
+// MQTT Parameters
+
+#define MQTT_BROKER "w.x.y.z"
+#define MQTT_CLIENTID "EntrHAnce"
+#define MQTT_USERNAME "your-mqtt-username"
+#define MQTT_PASSWORD "your-mqtt-password"
+#define MQTT_TOPICBASE "entry"
+
+// Enable/Disable Entry Monitoring
+
+#define ENTRY1_ENABLE true
+#define ENTRY2_ENABLE false
+#define ENTRY3_ENABLE false
+#define ENTRY4_ENABLE false
+#define ENTRY5_ENABLE false
+#define ENTRY6_ENABLE false
+#define ENTRY7_ENABLE false
+#define ENTRY8_ENABLE false
+
+// Entry Aliases
+
+#define ENTRY1_ALIAS "Entry 1"
+#define ENTRY2_ALIAS "Entry 2"
+#define ENTRY3_ALIAS "Entry 3"
+#define ENTRY4_ALIAS "Entry 4"
+#define ENTRY5_ALIAS "Entry 5"
+#define ENTRY6_ALIAS "Entry 6"
+#define ENTRY7_ALIAS "Entry 7"
+#define ENTRY8_ALIAS "Entry 8"
+
+// Entry Switch Logic
+
+#define ENTRY1_SWITCH_NO true
+#define ENTRY2_SWITCH_NO true
+#define ENTRY3_SWITCH_NO true
+#define ENTRY4_SWITCH_NO true
+#define ENTRY5_SWITCH_NO true
+#define ENTRY6_SWITCH_NO true
+#define ENTRY7_SWITCH_NO true
+#define ENTRY8_SWITCH_NO true
+
